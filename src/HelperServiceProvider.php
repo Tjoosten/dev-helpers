5d2fe9fc9854b8fb8e6af725a6d1d29a7a9a63ab
Tim Joosten <Tjoosten3@gmail.com>
2018-01-13 07:16:59 +0100

    Update

--- src/HelperServiceProvider.php
+++ src/HelperServiceProvider.php
@@ -1,3 +1,37 @@
 <?php
 
 namespace Misfits\Helpers\Development;
+
+use Illuminate\Support\ServiceProvider;
+
+/**
+ * Class HelperServiceProvider
+ *
+ * @package Misfits\Helpers\Development
+ */
+class HelperServiceProvider extends ServiceProvider
+{
+    /**
+     * Bootstrap the application services
+     *
+     * @return void
+     */
+    public function boot(): void
+    {
+        if ($this->app->runningInConsole()) {
+            $this->publishes([
+                __DIR__ . '/config/dev-helpers' => config_path('dev-helpers.php'),
+            ], 'config');
+        }
+    }
+
+    /**
+     * Register the applicatiàon services.
+     *
+     * @return void
+     */
+    public function register(): void
+    {
+        //
+    }
+}
